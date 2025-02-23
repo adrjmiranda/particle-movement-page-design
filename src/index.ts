2685b5098b7533c910e49f9d53753001f72efe06
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-23 11:54:09 -0300

    style: added animation for text content

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("It's working!");
+/**
+ * Scroll Reveal Animation
+ */
+import ScrollReveal from 'scrollreveal';
+
+const textContentItems: Array<string> = ['title', 'subtitle', 'btn'];
+
+for (let i = 0; i < textContentItems.length; i++) {
+	ScrollReveal().reveal(`.${textContentItems[i]}`, {
+		reset: false,
+		mobile: true,
+		opacity: 0,
+		origin: 'bottom',
+		distance: '30%',
+		easing: 'ease-in-out',
+		duration: 1750,
+		delay: i * 350,
+	});
+}
